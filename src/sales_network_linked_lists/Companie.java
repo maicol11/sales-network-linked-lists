90fd85efa22040e2c92c09ee63b6526fa9415896
Maicol Stiven Mancera Reinosa <maicol.mancera@construyored.com>
2020-10-09 23:02:01 -0500

    Proyecto

--- src/sales_network_linked_lists/Companie.java
+++ src/sales_network_linked_lists/Companie.java
@@ -20,14 +20,9 @@ public class Companie {
 		sellers.addSeller(s);
 	}
 	
-	
-	float sellerBalance2(Seller seller) {
-		
+	float sellerBalance2(Seller seller) {	
 		float generalBalance = membershipInitialFee - ((membershipInitialFee*membershipDiscountPercentaje) / 100);
-		
 		float balance = generalBalance;
-
-		
 		Seller tmpLeft = seller.left;
 		if(tmpLeft != null) {
 			float leftCommission = 0;
@@ -38,7 +33,6 @@ public class Companie {
 			float tmpBalance = generalBalance + leftCommission;
 			balance += (tmpBalance*25)/100;		
 		}
-		
 		Seller tmpRight = seller.right;
 		if(tmpRight != null) {
 			float rightCommission = 0;
@@ -46,142 +40,99 @@ public class Companie {
 				rightCommission += (generalBalance*commissionProfitPercentaje) / 100;
 				tmpRight = tmpRight.right;
 			}
-			float tmpBalanceRight = generalBalance + rightCommission;
-			
+			float tmpBalanceRight = generalBalance + rightCommission;	
 			balance += (tmpBalanceRight*25)/100;		
 		}
-		
 		return balance;	
 	}
 	
 	void totalBalance(Seller seller) {
-		
 		float balance = sellerBalance2(seller);	
 		Seller tmpright = seller.right;
-		float balance2 = 0;
+		float balanceRight = 0;
 		while(tmpright != null) {
-			balance2 += sellerBalance2(tmpright);
+			balanceRight += sellerBalance2(tmpright);
 			tmpright = tmpright.right;	
 		}
-		
 		Seller tmpleft = seller.left;
-		float balance3 = 0;
+		float balanceLeft = 0;
 		while(tmpleft != null) {
-			balance3 += sellerBalance2(tmpleft);
-			
+			balanceLeft += sellerBalance2(tmpleft);
 			tmpleft = tmpleft.left;
 		}
-		float total = balance + balance2 + balance3;
-		
+		float total = balance + balanceRight + balanceLeft;
 		System.out.println("total: " + total);
-		
 	}
 	
 	float biggerBalance(Seller seller) {
-		
 		float balance = sellerBalance2(seller);
-		
 		Seller tmpright = seller.right;
-		
-		float balance2 = 0;
 		float tmp = balance;
 		while(tmpright != null) {
 			float balancetmp = sellerBalance2(tmpright);
-			
 			if(balancetmp > tmp ) {
-				
 				tmp = balancetmp;
 			}
-			tmpright = tmpright.right;
-			
+			tmpright = tmpright.right;	
 		}
-		
 		Seller tmpleft = seller.left;
-
 		while(tmpleft != null) {
-			float balance3 = sellerBalance2(tmpleft);
-			if(balance3 > tmp ) {
-				
-				tmp = balance3;
+			float balanceLeft = sellerBalance2(tmpleft);
+			if(balanceLeft > tmp ) {
+				tmp = balanceLeft;
 			}
-			tmpleft = tmpleft.left;
-			
+			tmpleft = tmpleft.left;	
 		}
-
-		
 		return tmp;
-		
 	}
 	
 	float smallBalance(Seller seller) {
-		
 		float balance = sellerBalance2(seller);
-		
 		Seller tmpright = seller.right;
-
 		float tmp = balance;
 		while(tmpright != null) {
 			float balancetmp = sellerBalance2(tmpright);
-			
 			if(balancetmp < tmp ) {
-				
 				tmp = balancetmp;
 			}
 			tmpright = tmpright.right;
-			
 		}
-		
 		Seller tmpleft = seller.left;
-
 		while(tmpleft != null) {
-			float balance3 = sellerBalance2(tmpleft);
-			if(balance3 > tmp ) {
-				
-				tmp = balance3;
+			float balanceLeft = sellerBalance2(tmpleft);
+			if(balanceLeft > tmp ) {
+				tmp = balanceLeft;
 			}
-			tmpleft = tmpleft.left;
-			
+			tmpleft = tmpleft.left;	
 		}
-
 		return tmp;
 		
 	}
 	
 	void allNetworkBiggerBalance() {
 		SellerNode tmp = sellers.head;
-		
 		float bigger = 0;
 		while(tmp != null) {
 			float prueba = biggerBalance(tmp.value);
 			if(prueba > bigger) {
-				bigger = prueba;
-				
+				bigger = prueba;	
 			}
-			
 			tmp = tmp.next;
 		}
-		
 		System.out.println("Balance mayor: " + bigger);
 	}
 	
 	void allNetWorkSmallerBalance() {
 		SellerNode tmp = sellers.head;
-		
 		float small = smallBalance(tmp.value);
 		while(tmp != null) {
 			float prueba = smallBalance(tmp.value);
 			if(prueba < small) {
 				small = prueba;
-				
 			}
-			
 			tmp = tmp.next;
 		}
-		
 		System.out.println("Balance menor: " + small);
 	}
-	
-	
-	
 	
 }
--- src/sales_network_linked_lists/Program.java
+++ src/sales_network_linked_lists/Program.java
@@ -3,14 +3,19 @@ package sales_network_linked_lists;
 public class Program {
 
 	public static void main(String[] args) {
-		Companie c1 = new Companie("My company", 1000, 10, 25);
-		Seller s2 = new Seller("Maria");
-		Seller s1R1 = new Seller("Seller R1");
-		Seller s1R2 = new Seller("Seller R2");
-		Seller s1R1R1 = new Seller("s1R1R1");
-		Seller s1R1R2 = new Seller("s1R1R2");
+		Companie c1 	= new Companie("My company", 1000, 10, 25);
+		Seller s2 		= new Seller("Maria");
+		Seller s1R1 	= new Seller("Pedro");
+		Seller s1R2 	= new Seller("Juan");
+		Seller s1R1R1 	= new Seller("Carlos");
+		Seller s1R1R2 	= new Seller("Miguel");
+		Seller s1R1L2 	= new Seller("Luis");
+		Seller r1l1   	= new Seller("Maicol");
+		Seller r1l2   	= new Seller("Cristian");
+		Seller s1R2R2   = new Seller("Alejandra");
+		Seller s1R2R3   = new Seller("Helena");
 		
-		Seller segundo = new Seller("segunda network");
+		Seller segundo 	= new Seller("segunda network");
 		
 		c1.addSeller(s2);
 		
@@ -19,14 +24,18 @@ public class Program {
 		//s1.right = s2;
 		
 		s2.right = s1R1R1;
-		s2.left = s1R1;
+		s2.left  = s1R1;
 		
-		s1R1R1.right = s1R2;
+		s1R1R1.right 	= s1R2;
+		s1R2.right  	= s1R1R2;
+		s1R2.left 		= s1R1L2;
+		s1R1L2.left 	= r1l1;
+		r1l1.right 		= r1l2;
+		r1l2.left 		= s1R2R2;
+		r1l2.right 		= s1R2R3;
 		
 		System.out.println(s2.name + " balance: " + c1.sellerBalance2(s2));
-		
 		c1.totalBalance(s2);
-		
 		c1.allNetworkBiggerBalance();
 		c1.allNetWorkSmallerBalance();
 		
