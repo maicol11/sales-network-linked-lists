283578d3fc2f74cb83250993bc6ede08daa8a0de
Maicol Stiven Mancera Reinosa <maicol.mancera@construyored.com>
2020-10-09 23:48:40 -0500

    correcion lista

--- src/sales_network_linked_lists/SellerList.java
+++ src/sales_network_linked_lists/SellerList.java
@@ -23,58 +23,5 @@ public class SellerList {
 		}
 	}
 	
-	void showAll() {
-		SellerNode tmp = head;
-		System.out.println("Los nombres de los venderons son:");
-		while(tmp != null) {
-			System.out.println(tmp.value.name);
-			tmp = tmp.next;
-		}
-	}
-	
-	void bestSeller() {
-		SellerNode tmp = head;
-		float totalSale = tmp.value.totalSales;
-		SellerNode tmp2 = head;
-		while(tmp != null) {
-			tmp = tmp.next;
-			if(tmp != null) {
-				if(totalSale < tmp.value.totalSales) {
-					totalSale = tmp.value.totalSales;
-					tmp2 = tmp;
-				}	
-			}else {
-				System.out.println("El vendedor con más ventas en la compañia es: "+tmp2.value.name+" ("+tmp2.value.totalSales+")");
-			}
-		}
-	}
-	
-	void totalSales() {
-		SellerNode tmp = head;
-		float tSales = 0f;
-		while(tmp != null) {
-			tSales +=  tmp.value.totalSales;
-			tmp = tmp.next;
-		}	
-		System.out.println("El total de ventas es: " + tSales);
-	}
-	
-	void showAllTwo() {
-		SellerNode tmp = head;
-		System.out.println("Los nombres de los venderons right son:");
-		while(tmp != null) {
-			System.out.println(tmp.value.name);
-			
-			Seller tmpRight = tmp.value.right;
-			
-			while(tmpRight != null) {
-				System.out.println("	Nombre right: " + tmpRight.name);
-				
-				tmpRight = tmpRight.right;
-			}
-			
-			tmp = tmp.next;
-		}
-	}
 	
 }
